f0321061198c70b1781695a6b01b74c166d77e85
robertoooc <robertooo.c48@gmail.com>
2023-05-12 21:42:50 -0700

    checks theres a discord token and mongod uri or else fails

---
+++ src/ events/onInteraction.ts
@@ -0,0 +1,6 @@
+import { Interaction } from "discord.js";
+
+export const onInteraction = async (interaction: Interaction) => {
+  if (interaction.isCommand()) {
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import { IntentOptions } from "./config/IntentOptions";
 import { Client } from "discord.js";
 import { dbConnect } from "./db/dbConnect";
-// import { validateEnv } from "./utils/validateEnv";
-
+import { validateEnv } from "./utils/validateEnv";
+import { onInteraction } from "./ events/onInteraction";
 (async () => {
-  // if (!validateEnv()) return;
+  if (!validateEnv()) return;
 
-  const BOT = new Client({intents: IntentOptions});
-
+  const BOT = new Client({ intents: IntentOptions });
 
   BOT.on("ready", () => console.log("Connected to Discord!"));
-
-   await dbConnect();
+  BOT.on(
+    "interactionCreate",
+    async (interaction) => await onInteraction(interaction)
+  );
+  await dbConnect();
 
   await BOT.login(process.env.BOT_TOKEN);
 })();
