653cf9397b9c83da680d634325d6df9b89842b68
robertoooc <robertooo.c48@gmail.com>
2023-05-16 17:54:52 -0700

    implemented feature to add emoji reaction to texts, need to fix types though

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -1,7 +1,7 @@
-import { SlashCommandBuilder } from "@discordjs/builders";
+import { SlashCommandBuilder, } from "@discordjs/builders";
 import { CommandInt } from "../types/types";
-
-export const test: CommandInt = {
+import { MessageEmbed } from "discord.js";
+export const test: any = {
   data: new SlashCommandBuilder()
     .setName("test")
     .setDescription("type anything")
@@ -11,10 +11,26 @@ export const test: CommandInt = {
         .setDescription("the message goes here")
         .setRequired(true)
     ),
-  run: async (interaction) => {
+  run: async (interaction:any) => {
     await interaction.deferReply();
     const { user } = interaction;
     const text = interaction.options.getString("message", true);
-    console.log(user, text)
+    console.log(user, text);
+    const sendMessage = new MessageEmbed();
+    sendMessage.setTitle("test");
+    sendMessage.setDescription(text);
+    sendMessage.setAuthor({
+      name: user.tag,
+      // iconURL: user.displayAvatarURL(),
+    });
+    
+		const message = await interaction.editReply({ content: 'You can react with Unicode emojis!', fetchReply: true })
+    await message.react('🛑')
+    // message.reply
+
+    // sendMessage.addField("Day",new Date(Date.now()).toLocaleString(), true);
+    // await interaction.reply({embeds:[sendMessage]})
+    
   },
 };
+
