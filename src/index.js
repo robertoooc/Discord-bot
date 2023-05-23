1d647e79ec2eb701f8366906e6291aa17e0adfb0
robertoooc <robertooo.c48@gmail.com>
2023-05-23 14:25:10 -0700

    user is able to update status of job now

--- src/index.js
+++ src/index.js
@@ -32,10 +32,7 @@ const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
 client.on("ready", () => console.log(`${client.user.tag} has logged in!`));
 
 client.on("interactionCreate", async (interaction) => {
-  // console.log(interaction);
-
   if (interaction.isChatInputCommand()) {
-    // console.log(interaction);
     if (interaction.commandName === "newjob") {
       const modal = new ModalBuilder()
         .setTitle("New Job Posting")
@@ -63,7 +60,9 @@ client.on("interactionCreate", async (interaction) => {
               .setRequired(false)
           )
         );
+
       await interaction.showModal(modal);
+
       try {
         const modalResponse = await interaction.awaitModalSubmit({
           filter: (i) =>
@@ -72,10 +71,6 @@ client.on("interactionCreate", async (interaction) => {
         });
 
         if (modalResponse.isModalSubmit()) {
-          console.log(
-            modalResponse.fields.getTextInputValue("jobPostingInput")
-          );
-
           const actionRowComponent = new ActionRowBuilder().setComponents(
             new SelectMenuBuilder().setCustomId("job_options").setOptions([
               { label: "Waiting Response ⏳", value: "waiting" },
@@ -83,18 +78,19 @@ client.on("interactionCreate", async (interaction) => {
               { label: "Rejected ❌", value: "rejected" },
             ])
           );
+
           const response = await modalResponse.reply({
             content: "Please select the status of your job posting",
             components: [actionRowComponent],
             fetchReply: true,
           });
+
           const collectorFilter = (i) => i.user.id === interaction.user.id;
+
           const status = await response.awaitMessageComponent({
             filter: collectorFilter,
             time: 60000,
           });
-          console.log(status.values[0]);
-          console.log(interaction);
 
           await status.reply({
             content: "Your submission was received successfully!",
@@ -103,6 +99,7 @@ client.on("interactionCreate", async (interaction) => {
           const findUser = await User.findOne({
             discordId: interaction.user.id,
           });
+
           if (findUser) {
             const job = {
               name: modalResponse.fields.getTextInputValue("jobPostingInput"),
@@ -110,6 +107,7 @@ client.on("interactionCreate", async (interaction) => {
               link: modalResponse.fields.getTextInputValue("jobPostingLink"),
               notes: modalResponse.fields.getTextInputValue("notes"),
             };
+
             findUser.jobs.push(job);
             await findUser.save();
           } else {
@@ -144,6 +142,7 @@ client.on("interactionCreate", async (interaction) => {
         const findUser = await User.findOne({
           discordId: interaction.user.id,
         });
+
         if (!findUser)
           interaction.reply({ content: "You have no job postings to update!" });
 
@@ -153,21 +152,13 @@ client.on("interactionCreate", async (interaction) => {
             value: job.id,
           };
         });
+
         const selectJobPosting = new ActionRowBuilder().setComponents(
           new SelectMenuBuilder()
             .setCustomId("select_job_posting")
             .setOptions(jobs)
         );
 
-        // const actionRowComponent = new ActionRowBuilder().setComponents(
-        //   new SelectMenuBuilder().setCustomId("job_options").setOptions([
-        //     { label: "Waiting Response ⏳", value: "waiting" },
-        //     { label: "Offer/Interview ✅", value: "accepted" },
-        //     { label: "Rejected ❌", value: "rejected" },
-        //   ])
-        // );
-        // console.log(actionRowComponent);
-        // console.log(selectJobPosting);
         const response = await interaction.reply({
           content: "Please select the status of your job posting",
           components: [selectJobPosting],
@@ -175,14 +166,44 @@ client.on("interactionCreate", async (interaction) => {
         });
 
         const collectorFilter = (i) => i.user.id === interaction.user.id;
+
         const status = await response.awaitMessageComponent({
           filter: collectorFilter,
           time: 60000,
         });
+
         console.log(status.values[0]);
+
+        const job = findUser.jobs.find((job) => job.id == status.values[0]);
+        console.log(job);
+
+        const updateStatus = new ActionRowBuilder().setComponents(
+          new SelectMenuBuilder().setCustomId("update_status").setOptions([
+            { label: "Waiting Response ⏳", value: "waiting" },
+            { label: "Offer/Interview ✅", value: "accepted" },
+            { label: "Rejected ❌", value: "rejected" },
+          ])
+        );
+
+        const updateResponse = await status.reply({
+          content: "Please update the status of your job posting",
+          components: [updateStatus],
+          fetchReply: true,
+        });
+
+        const udpateResponse = await updateResponse.awaitMessageComponent({
+          filter: collectorFilter,
+          time: 60000,
+        });
+        console.log(udpateResponse.values[0]);
+
+        job.status = udpateResponse.values[0];
+        await findUser.save();
+
+        await udpateResponse.reply({content: "Your job posting has been updated!"})
       } catch (err) {
         console.log(err);
-        interaction.reply({ content: "Something went wrong!" });
+        interaction.editReply({ content: "Something went wrong!" });
       }
     }
   }
