891d82cc97995f581a2918a41925fc3029d1e5a0
robertoooc <robertooo.c48@gmail.com>
2023-05-24 21:52:17 -0700

    progress ?

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
+require('dotenv').config();
 
 export function dbConnect() {
   const dbName = "DiscordBot";
