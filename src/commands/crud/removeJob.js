382e97fd7515ca3d0bcfc0ebf15687f1980ca511
robertoooc <robertooo.c48@gmail.com>
2023-06-02 19:18:08 -0700

    fixed a typo on deploy-commands file

--- src/commands/crud/removeJob.js
+++ src/commands/crud/removeJob.js
@@ -1,49 +1,53 @@
-const {
-  SlashCommandBuilder,
-  ActionRowBuilder,
-  EmbedBuilder,
-  ButtonBuilder,
-} = require("discord.js");
-const db = require("../../models");
+const { SlashCommandBuilder, ActionRowBuilder } = require("discord.js");
 const { SelectMenuBuilder } = require("@discordjs/builders");
+const db = require("../../models");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("removejob")
-    .setDescription("Remove a Job Posting"),
+    .setDescription("Remove Job Posting"),
   async execute(interaction) {
-    const user = await db.User.findOne({
-      discordId: interaction.user.id,
-    });
-    if (!user || user.jobs.length == 0) {
-      return interaction.reply({
-        content: "You have no job postings to remove!",
+    try {
+      const findUser = await db.User.findOne({
+        discordId: interaction.user.id,
       });
+
+      if (!findUser || findUser.jobs == null) {
+        interaction.reply({ content: "You have no job postings to update!" });
+      } else {
+        const jobs = findUser.jobs.map((job) => {
+          return {
+            label: job.name,
+            value: job.id,
+          };
+        });
+
+        const selectJobPosting = new ActionRowBuilder().setComponents(
+          new SelectMenuBuilder()
+            .setCustomId("remove_job_posting")
+            .setOptions(jobs)
+        );
+
+        const response = await interaction.reply({
+          content: "Please select the status of your job posting",
+          components: [selectJobPosting],
+          fetchReply: true,
+        });
+
+        const collectorFilter = (i) => i.user.id === interaction.user.id;
+
+        const status = await response.awaitMessageComponent({
+          filter: collectorFilter,
+          time: 60000,
+        });
+
+        console.log(status.values[0]);
+
+        const job = findUser.jobs.find((job) => job.id == status.values[0]);
+        console.log(job);
+      }
+    } catch (err) {
+      console.log(err);
+      interaction.editReply({ content: "Something went wrong!" });
     }
-
-    const jobs = user.jobs.map((job) => {
-      return {
-        label: job.name,
-        value: job.id,
-      };
-    });
-
-    const selectJobPosting = new ActionRowBuilder().setComponents(
-      new SelectMenuBuilder().setCustomId("removo_job_posting").setOptions(jobs)
-    );
-
-    const response = await interaction.reply({
-      content: "Please select the status of your job posting",
-      components: [selectJobPosting],
-      fetchReply: true,
-    });
-
-    const collectorFilter = (i) => i.user.id === interaction.user.id;
-
-    const status = await response.awaitMessageComponent({
-      filter: collectorFilter,
-      time: 60000,
-    });
-
-    console.log(status.values[0]);
   },
 };
