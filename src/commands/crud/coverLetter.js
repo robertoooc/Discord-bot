8fd8ca4d9f4fdec828767dd3b5cda3f1af0b6ad4
robertoooc <robertooo.c48@gmail.com>
2023-06-15 12:01:48 -0700

    sucessfully pinged openai api, needs tuning

--- src/commands/crud/coverLetter.js
+++ src/commands/crud/coverLetter.js
@@ -6,38 +6,29 @@ const {
   TextInputStyle,
 } = require("discord.js");
 require(`dotenv`).config();
-const API_KEY = process.env.API_KEY;
-const axios = require("axios");
-// const db = require("../../models");
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 async function generateCoverLetter(resume, jobPosting) {
-  const prompt = `Write a cover letter for a job application. Tone: conversational, startan, use less corporate jargon.
-  The Resume: ${resume}, The Job Posting: ${jobPosting}
-  `;
-
+  const prompt = `Write a cover letter for a job application. Tone: conversational, startan, use less corporate jargon.\nThe Resume: ${resume}\nThe Job Posting: ${jobPosting}`;
   try {
-    const response = await axios.post(
-      "https://api.openai.com/v1/engines/davinci-codex/completions",
-      {
-        prompt: prompt,
-        max_tokens: 200,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${API_KEY}`,
-        },
-      }
-    );
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      temperature: 0.3,
+      max_tokens: 500,
+    });
 
-    const generatedText = response.data.choices[0].text;
-    console.log(generatedText);
+    console.log(response.data.choices[0].text.trim());
   } catch (error) {
-    console.error("Error:", error);
+    console.log("Error:", error.response.data.error);
   }
 }
 
-// Call the function to generate the cover letter
-generateCoverLetter();
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("createcoverletter")
@@ -70,11 +61,13 @@ module.exports = {
           i.customId === "coverLetter" && i.user.id === interaction.user.id,
         time: 60000,
       });
-      console.log(modalResponse);
-      const resume = modalResponse.fields.getTextInputValue("resume");
-      const jobPosting = modalResponse.fields.getTextInputValue("jobPostingDescription");
-      console.log(resume,jobPosting)
-
+      if (modalResponse.isModalSubmit()) {
+        const resume = modalResponse.fields.getTextInputValue("resume");
+        const jobPosting = modalResponse.fields.getTextInputValue(
+          "jobPostingDescription"
+        );
+        await generateCoverLetter(resume, jobPosting);
+      }
     } catch (err) {
       console.log(err);
     }
