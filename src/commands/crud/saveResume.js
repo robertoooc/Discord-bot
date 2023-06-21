ab3f6069be97441f90b6521b0183656604daf0cc
robertoooc <robertooo.c48@gmail.com>
2023-06-20 17:46:25 -0700

    able to save resume to db, but when trying to display a character limit 0f 2000 is exceeded

--- src/commands/crud/saveResume.js
+++ src/commands/crud/saveResume.js
@@ -1,13 +1,62 @@
 const {
   SlashCommandBuilder,
   ActionRowBuilder,
-  EmbedBuilder,
-  ButtonBuilder,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
 } = require("discord.js");
 const db = require("../../models");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("saveresume")
     .setDescription("Don't want to paste your resume every time? Save it!"),
-  async execute(interaction) {},
+  async execute(interaction) {
+    const modal = new ModalBuilder()
+      .setTitle("Save Resume")
+      .setCustomId("saveResume")
+      .setComponents(
+        new ActionRowBuilder().setComponents(
+          new TextInputBuilder()
+            .setLabel("Enter Your Resume")
+            .setCustomId("resume")
+            .setStyle(TextInputStyle.Paragraph)
+            .setRequired(true)
+        )
+      );
+
+    await interaction.showModal(modal);
+    try {
+      const modalResponse = await interaction.awaitModalSubmit({
+        filter: (i) =>
+          i.customId === "saveResume" && i.user.id === interaction.user.id,
+        time: 300000,
+      });
+      if (modalResponse.isModalSubmit()) {
+        await modalResponse.reply({
+          content: "Saving Resume...",
+          ephemeral: true,
+        });
+        const resume = modalResponse.fields.getTextInputValue("resume");
+        const user = await db.User.findOne({
+          discordId: interaction.user.id,
+        });
+        if (user) {
+          user.resume = resume;
+          await user.save();
+        } else {
+          await db.User.create({
+            discordId: interaction.user.id,
+            username: interaction.user.username,
+            resume: resume,
+          });
+        }
+        await modalResponse.editReply({
+          content: `Your resume has been saved! \n ${resume}`,
+          ephemeral: true,
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  },
 };
